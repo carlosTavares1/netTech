4dfb0f9dc2f60e128c204536a73d4c4318c36191
Francisco Augusto Martin <francisco.martin@ambevtech.com.br>
2022-10-09 20:21:38 -0300

    add items and api requests

--- src/Banner.js
+++ src/Banner.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Request";
 
 function Banner() {
-    function truncate(string, n) {
-        return string?.length > n ? string.substr(0, n -1) + '...' : string;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchDAta() {
+      const request = await axios.get(requests.fetchNetflixOrinals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
     }
+    fetchDAta();
+  }, []);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
   return (
     <header
       className="banner"
       style={{
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Techologies</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{truncate(`This is a new technology`,150)}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
